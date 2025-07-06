8a7a1a144365f21725de986a6e5dd0d7d4647dda
@p.pokorsky <828@rucargo.net>
2025-07-06 18:50:59 +0300

    theme

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from "./router";
 import "vuetify/styles";
 import "@/assets/styles/main.scss";
 import i18n from "./i18n";
-import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
